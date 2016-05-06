030fdeca9f6470390a3cd733c5604ab387837bd6
Susan Rees <susan@reeshome.org>
2016-05-05 20:56:46 -0700

    session six in-class exercises

--- learning_journal/learning_journal/views.py
+++ learning_journal/learning_journal/views.py
@@ -1,22 +1,44 @@
 from pyramid.response import Response
 from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPNotFound
 
 from sqlalchemy.exc import DBAPIError
 
 from .models import (
     DBSession,
     MyModel,
+    Entry,
     )
 
+# @view_config(route_name='home', renderer='templates/mytemplate.pt')
+# def my_view(request):
+#     try:
+#         one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
+#     except DBAPIError:
+#         return Response(conn_err_msg, content_type='text/plain', status_int=500)
+#     return {'one': one, 'project': 'learning_journal'}
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    try:
-        one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
-    except DBAPIError:
-        return Response(conn_err_msg, content_type='text/plain', status_int=500)
-    return {'one': one, 'project': 'learning_journal'}
+@view_config(route_name='home', renderer='string')
+def index_page(request):
+    entries = Entry.all()
+    return {'entries': entries}
 
+@view_config(route_name='detail', renderer='templates/detail.jinja2')
+def view(request):
+    this_id = request.matchdict.get('id', -1)
+    entry = Entry.by_id(this_id)
+
+    if not entry:
+        return HTTPNotFound()
+    return {'entry': entry}
+
+@view_config(route_name='action', match_param='action=create', renderer='string')
+def create(request):
+    return 'create page'
+
+@view_config(route_name='action', match_param='action=edit', renderer='string')
+def create(request):
+    return 'edit page'
 
 conn_err_msg = """\
 Pyramid is having a problem using your SQL database.  The problem
--- simple-mvc/view.py
+++ simple-mvc/view.py
@@ -1,5 +1,34 @@
+from model import widget
+from template import Template
 
 class View(object):
 
-    def dispatch(request):
-        return "Make this work"
+    @classmethod
+    def dispatch(cls.request):
+        path = request.path
+        name = path.strip('/').split('/')[0]
+
+        widget = widget.get_widget(name)
+
+        method = {
+            'GET': cls.get,
+            'POST': cls.post,
+        }.get(request.method)
+
+        return method(request, widget)
+
+    @classmethod
+    def get(cls, request, widget):
+        return cls.render_widget(widget)
+
+    @classmethod
+    def post(cls, request, widget):
+        new_value = request.fields.getvalue('value')
+
+        widget.set_value(new_value)
+
+        return cls.render_widget(widget)
+
+    @classmethod
+    def render_widget(cls, widget):
+        return Template.render({'value': widget.get_value()})
